c67b4b2011f876f2244b2d738943c83951d3b24b
Emilio Ober <emilio@NonnaPC.emilioo.de>
2026-08-05 15:57:04 +0200

    Added \n in print

--- kernel.c
+++ kernel.c
@@ -1,5 +1,8 @@
+#define WIDTH 80
 
+#define HEIGHT 40
 void print(char *value, char *video);
+int write = 0;
 
 void main(){
 	//My kernel!!!
@@ -7,16 +10,33 @@ void main(){
 	char *video =(char*)0xB8000;
 
 	print("Hallo Welt",video);
+	print("new\nline", video);
 	while(1){}
 }
+/*
+void set_cursor(int pos){
+	int *low = (int*)0x3D4;
+	int *high = (int*)0x3D5;
 
+	low=0x01;
+	high=0x00;
+}
+*/
+//print
 void print(char *value, char *video){
-	int d = 0;
 	for(int i=0;value[i]!=0;i++){
-		video[d]=value[i];
-		d++;
-		video[d]=0x07;
-		d++;
+		if(value[i]=='\n'){
+			//*2 beacuse every char is 2 bytes (char, color)
+			//setting write to a line start and adding a new line
+			write= (write - (write%(WIDTH*2)))+(WIDTH*2);
+			continue;
+		}
+		//writing char
+		video[write]=value[i];
+		write++;
+		//writing color
+		video[write]=0x07;
+		write++;
 	}
 	
 }
